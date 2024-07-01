1edecac4ee9fe96a3306747d2281bfa0226092d9
Devang-shah2023 <shahdevang1232@gmail.com>
2024-07-01 15:01:18 +0530

    javascript basics-part1

---
+++ Frontend course/JavascriptBasics-23/index.js
@@ -0,0 +1,77 @@
+        //line below is used to print in console window
+        console.log('Namaste Dunia version4');
+
+        // let a=true;
+        // console.log(a);
+
+        // const num=12;
+        // // num=13;
+
+        // let lastname='Babbar';
+        // console.log(lastname);
+
+        // lastname=3;
+        // console.log(lastname);
+
+        // lastname=true;
+        // console.log(lastname);
+
+        // console.log(5!==3);
+
+        // let age=27;
+        // let status=(age>18)?'Yes Vote':'No Vote';
+
+        // console.log(status);
+
+        // let marks=98;
+
+        // if(marks>=90){
+        //         console.log('A');
+        // }
+        // else if(marks>=80){
+        //         console.log('B');
+        // }
+        // else if(marks>=70){
+        //         console.log('C');
+        // }
+        // else if(marks>=60){
+        //         console.log('D');
+        // }
+        // else{
+        //         console.log('E')
+        // }
+
+        // let num=2;
+
+        // switch(num){
+        //         case 1: console.log('A');
+        //         break;
+        //         case 2: console.log('B');
+        //         break;
+        //         case 3: console.log('C');
+        //         break;
+        //         default: console.log('D');
+                
+        // }
+
+        for(let i=0; i<5; i=i+1){
+                console.log("babbar"+i);
+        }
+
+        let x=1;
+
+        while(x<=5){
+                console.log(x);
+                x++;
+        }
+
+        let y=1;
+
+        do{
+                console.log(y);
+                y++
+        }
+        while(y<10);
+
+
+
