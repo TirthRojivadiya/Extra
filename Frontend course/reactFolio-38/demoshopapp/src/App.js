3f27578541fd37fdb99cec6348be46f7ceb1b356
Devang-shah2023 <shahdevang1232@gmail.com>
2024-10-05 14:59:29 +0530

    Notes App Project

--- Frontend course/reactFolio-38/demoshopapp/src/App.js
+++ Frontend course/reactFolio-38/demoshopapp/src/App.js
@@ -2,41 +2,36 @@ import logo from './logo.svg';
 import './App.css';
 import Item from './components/Item';
 import ItemDate from './components/ItemDate';
-function App() {
-  const response=[
+const App= ()=> {
+  const Products =[
     {
-      itemName:"Nirma",
-      itemDate:"20",
-      itemMonth:"June",
-      itemYear:"1998"
+      id:"p1",
+      title:"Nirma",
+      amount:100,
+      date:new Date(2021,8,10),
     },
     {
-      itemName:"Nirma2",
-      itemDate:"202",
-      itemMonth:"June2",
-      itemYear:"199982"
+      id:"p2",
+      title:"Surf Excel",
+      amount:200,
+      date:new Date(2021,2,2),
     },
     {
-      itemName:"Nirma3",
-      itemDate:"203",
-      itemMonth:"June3",
-      itemYear:"19983"
+      id:"p3",
+      title:"Tide",
+      amount:130,
+      date:new Date(2021,12,28),
+    },
+    {
+      id:"p4",
+      title:"Maggi",
+      amount:450,
+      date:new Date(2021,5,5),
     }
   ];
   return (
     <div>
-    
-    <Item name={response[0].itemName} ></Item>
-    <ItemDate day={response[0].itemDate} month={response[0].itemMonth} year={response[0].itemYear}></ItemDate>
-
-    <Item name={response[1].itemName} ></Item>
-    <ItemDate day={response[1].itemDate} month={response[1].itemMonth} year={response[1].itemYear}></ItemDate>
-
-    <Item name={response[2].itemName} ></Item>
-    <ItemDate day={response[2].itemDate} month={response[2].itemMonth} year={response[2].itemYear}></ItemDate>
-    <div classname="App">
-      Hello jee
-    </div>
+       <Products items={products}/>
     </div>
   );
 }
